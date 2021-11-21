b1b2d69fe83fef912f6a8b61ba15e83599422af1
kbberker <kbbuk90@gmail.com>
2021-11-21 20:12:44 +0000

    chore: fix eslint issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Rate this product</h1>
+      <form>
+        <label htmlFor="name">
+          Name:
+          <input type="text" name="name" />
+        </label>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
